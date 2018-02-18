de7aa23c8eca40c674a18f6fad72af0565f21c2e
saif <syedabdulrahim4@gmail.com>
2018-02-18 22:04:48 +0530

    modified:   index.html

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
-var scores,roundScore,activePlayer,dice;
+var scores,roundScore,activePlayer;
 
 
 
 
 scores=[0,0];
 roundScore=0;
-activePlayer=0;
-dice =Math.floor(Math.random()*6)+1;
-console.log(dice);
-document.querySelector("#score-0").textContent= dice;
+activePlayer=1;
+
+//console.log(dice);
+//document.querySelector("#current-0").textContent= dice;
+//document.querySelector("#current-" + activePlayer).innerHTML='<em>'+dice+'</em>';//SETTER
+var test= document.querySelector("#current-"+activePlayer).textContent;//GETTER
+
+
+/*function btn()
+{
+    
+    //do something here
+}
+*/
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    
+    
+    
+    //generate the random number
+    var dice =Math.floor(Math.random()*6)+1;
+   // 2.Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display='block';
+    diceDOM.src='dice-' + dice + '.png';
+    
+});
+
+
+
